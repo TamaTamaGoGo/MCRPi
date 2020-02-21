b200a5f157bdbb33ed9e57bab1cdd90ac2ccd34d
TamaTamaGoGo <tamatama@tensi.org>
2020-02-21 20:43:02 +0900

    Update SetupMCPi.py

--- SetupMCPi.py
+++ SetupMCPi.py
@@ -1,6 +1,8 @@
 from tkinter import messeagebox
 import subprocess
 
+subprocess.call('mkdir Download MCPi')
 subprocess.call('mkdir MCPi')
 subprocess.call('cd MCPi')
+subprocess.call('')
 #subprocess.call('wget https://github.com......')
