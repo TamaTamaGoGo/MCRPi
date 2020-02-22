be256bb98c9ec24787edc828d5ea96a95ebf43b2
TamaTamaGoGo <tamatama@tensi.org>
2020-02-22 20:27:10 +0900

    Update SetupMCPi.py

--- SetupMCPi.py
+++ SetupMCPi.py
@@ -7,9 +7,8 @@ try:
     subprocess.check_call('mkdir MCPi')
     subprocess.check_call('./SetupMCPi')
     subprocess.check_call('cd MCPi')
-    subprocess.check_call('')
+#   subprocess.check_call('')
     #subprocess.call('wget https://github.com......')
-    
 except:
     print("Setup failed")
     p=subprocess.Popen('a.exe args',shell=True)
