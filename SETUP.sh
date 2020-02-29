1d6126615f069f2b8d1aa3e38f6b8560b7441a06
TamaTamaGoGo <tamatama@tensi.org>
2020-02-29 14:35:04 +0900

    Update SETUP.sh

--- SETUP.sh
+++ SETUP.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
+echo -n 'Enter your directory to download> '
+read DIR
+echo $DIR
 
+echo 'Progress 1/3   ******------------'
 echo 'starting Setting MCPi'
 sudo su
 apt-get -y install python3
-echo 'Running MCPi'
-wget https://github.com/....
+
+echo 'Progress 2/3   ************------'
+echo 'Downloading and preparing SetupMCPi.py'
+wget https://github.com/TamaTamaGoGo/MCPi---MineCraft-for-raspberryPi/blob/master/Setup/SetupMCPi.py
+sudo chmod 777 SetupMCPi.py
+
+echo 'Progress 3/3   ****************'
+echo 'Running SetupMCPi.py'
