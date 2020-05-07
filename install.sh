ac7106544bcad1cde0984d287e44467a118e258a
TamaTamaGoGo <tamatama@tensi.org>
2020-05-07 23:12:46 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,27 +4,33 @@
 ##Information
 echo "MCPi project use Shell and Python(?~ and zenity"
 
-##pre zenity
+#pre zenity
 echo Installing zenity ; sleep 1
 sudo apt-get install -y zenity ; sleep 1
 
-##Pre
-#zenity --progress --time-remaining --text='Install and Initalize MCPi \nIt only takes minutes \nPlease wait...'
+cd ${HOME}
+
+#Pre
+##zenity --progress --time-remaining --text='Install and Initalize MCPi \nIt only takes minutes \nPlease wait...'
 echo "5"
 mkdir MCPi
 echo "10"
 mkdir .minecraft
+mkdir Minecraft
 
-##Pre 2
-cd ${HOME}/MCPi
+#Pre 2
+cd ${HOME}/Minecraft
 
-##Downloading MCPi Core from GITHUB's Repository
-sudo wget 
+####Downloading MCPi Core from GITHUB's Repository -Nope
+#Downloading Minecraft Official Lancher
+sudo wget https://launcher.mojang.com/v1/objects/eabbff5ff8e21250e33670924a0c5e38f47c840b/launcher.jar
 echo "15"
 
 ##Install and Initalize mesa-utils
 sudo apt-get install -y mesa-utils
 echo "20"
+
+#Downloading 
 
 
 
