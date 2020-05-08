6b3b0c7ee836f998e722bf3e61f56d02aee2d7a0
TamaTamaGoGo <tamatama@tensi.org>
2020-05-08 23:40:04 +0900

    Update install32bit.sh

--- install32bit.sh
+++ install32bit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 (
+
 ##Information
 echo "MCRPi project use Shell and Python(?~ and zenity"
 
@@ -29,7 +30,7 @@ echo 'cd Minecraft ; wget https://launcher.mojang.com/v1/objects/eabbff5ff8e2125
 sudo chmod 777 download.sh
 ./download.sh
 echo "15"
-cd ..
+
 
 echo "# Install and Initalize mesa-utils"
 sudo apt-get install -y mesa-utils
@@ -38,11 +39,11 @@ echo "20"
 echo "# Downloading oracle java8 not using repo"
 if [ ! -f jdk-8u241-linux-arm32-vfp-hflt.tar.gz ]; then
 	echo 'cd jdk ; wget https://www.dropbox.com/s/h6smy7w7dsikdis/jdk-8u241-linux-arm32-vfp-hflt.tar.gz 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --auto-kill --title="Downloading File..."' > download.sh
+	sudo chmod 777 download.sh
 	./download.sh
 fi
 
 echo "25"
-cd ..
 
 echo "# Extract"
 sudo tar -zxf jdk-8u241-linux-arm32-vfp-hflt.tar.gz -C /jdk
@@ -60,10 +61,11 @@ echo "# Downloading Minecraft Server 1.15.2"
 mkdir MinecraftServer
 #cd MinecraftServer
 echo 'cd MinecraftServer ; wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --auto-kill --title="Downloading File..."' > download.sh
+sudo chmod 777 download.sh
 ./download.sh
 #/jdk/jdk1.8.0_241/bin/java -Xmx500M -Xms500M -jar minecraft_server.1.15.2.jar nogui
 echo "35"
-cd ..
+
 
 echo "# change GPU memory"
 sudo bash -c "echo gpu_mem=248>> /boot/config.txt"
