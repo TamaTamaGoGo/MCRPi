b6731583371e4b5de2e9c41d4fbd2695c5e78d55
TamaTamaGoGo <tamatama@tensi.org>
2020-05-10 21:52:00 +0900

    Update install32bit.sh

--- install32bit.sh
+++ install32bit.sh
@@ -8,6 +8,7 @@ echo "MCRPi project use Shell and Python(?~ and zenity"
 #pre zenity
 echo Installing zenity ; sleep 1
 sudo apt-get install -y zenity ; sleep 1
+dir=`pwd`
 
 echo "# Pre"
 ##zenity --progress --time-remaining --text='Install and Initalize MCPi \nIt only takes minutes \nPlease wait...'
@@ -26,9 +27,9 @@ echo "# Pre 2"
 
 ####Downloading MCPi Core from GITHUB's Repository -Nope
 echo "# Downloading Minecraft Official Lancher"
-echo 'cd Minecraft ; wget https://launcher.mojang.com/v1/objects/eabbff5ff8e21250e33670924a0c5e38f47c840b/launcher.jar 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --title="Downloading File..."' > download.sh
-sudo chmod 777 download.sh
-./download.sh
+#lxterminal --working-directory=${dir}/Minecraft -e wget https://launcher.mojang.com/v1/objects/eabbff5ff8e21250e33670924a0c5e38f47c840b/launcher.jar 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --auto-kill --title="Downloading File..." ; exit
+sudo chmod +x download1.sh
+./download1.sh
 echo "15"
 
 
@@ -38,9 +39,9 @@ echo "20"
 
 echo "# Downloading oracle java8 not using repo"
 if [ ! -f jdk-8u241-linux-arm32-vfp-hflt.tar.gz ]; then
-	echo cd jdk ; wget https://www.dropbox.com/s/h6smy7w7dsikdis/jdk-8u241-linux-arm32-vfp-hflt.tar.gz 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --title="Downloading File..." > download.sh
-	sudo chmod 777 download.sh
-	./download.sh
+#	lxterminal --working-directory=${dir}/jdk -e wget https://www.dropbox.com/s/h6smy7w7dsikdis/jdk-8u241-linux-arm32-vfp-hflt.tar.gz 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --auto-kill --title="Downloading File..."
+	sudo chmod +x download2.sh
+	./download2.sh
 fi
 
 echo "25"
@@ -60,9 +61,7 @@ echo "30"
 echo "# Downloading Minecraft Server 1.15.2"
 mkdir MinecraftServer
 #cd MinecraftServer
-echo 'cd MinecraftServer ; wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --title="Downloading File..."' > download.sh
-sudo chmod 777 download.sh
-./download.sh
+lxterminal --working-directory=${dir}/MinecraftServer -e wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --auto-kill --title="Downloading File..."
 #/jdk/jdk1.8.0_241/bin/java -Xmx500M -Xms500M -jar minecraft_server.1.15.2.jar nogui
 echo "35"
 
@@ -75,7 +74,7 @@ echo "40"
 
 )  |
 zenity --progress \
-	--time-remaining --text='Install and Initalize MCRPi \nIt only takes minutes \nPlease Wait...\n' --auto-kill --percentage=0
+	--time-remaining --text='Install and Initalize MCRPi \nIt only takes minutes \nPlease Wait...\n' --auto-kill --percentage=0 --title='install and Initalize MCRPi'
 
 if [ "$?" = -1 ] ; then
         zenity --error \
