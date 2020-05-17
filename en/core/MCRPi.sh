7c8ad91a82bdd9e46f6078a235d99a124f5461b9
TamaTamaGoGo <tamatama@tensi.org>
2020-05-17 23:00:51 +0900

    Update MCRPi.sh

--- en/core/MCRPi.sh
+++ en/core/MCRPi.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 echo '[OK] Boot MCRPi.sh'
-zenity --list --radiolist --column=Selected --column "ペット" True "Start Minecraft Launcher" False "Update MCRPi" False "Change user infomation" False "Exit"
+chose='zenity --list --radiolist --column=Selected --column "ペット" True "Start Minecraft Launcher" False "Update MCRPi" False "Change user infomation" False "Exit"'
 
