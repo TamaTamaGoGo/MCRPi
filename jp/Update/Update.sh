5cec7e8a6f7d6ddea834b43252946cd0fcc10598
TamaTamaGoGo <tamatama@tensi.org>
2020-07-13 23:06:50 +0900

    Update Update.sh
    Update a little

--- jp/Update/Update.sh
+++ jp/Update/Update.sh
@@ -2,3 +2,10 @@
 rm Latestversion.txt
 wget https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/Python-v.0.1/Update/jp/Latestverision.txt
 Latestversion=$(cat Latestversion.txt)
+Currentlyversion=$(cat Version.txt)
+if [ "$Latestversion" = "$Currentlyversion" ]; then
+    echo "MCRPi is already currentry version"
+    fi
+else
+    echo "MCRPi is now Up to Date"
+    fi
