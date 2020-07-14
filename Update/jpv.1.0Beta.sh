247e80605583f8a6a7740de3b3798372f8ed61a3
TamaTamaGoGo <tamatama@tensi.org>
2020-07-14 22:58:04 +0900

    Update jpv.1.0Beta.sh

--- Update/jpv.1.0Beta.sh
+++ Update/jpv.1.0Beta.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 echo "アップデート中です"
+echo "最新バージョンか確認しています"
+if [ "$latest_ver" = "v.1.0" ]; then
+    echo "最新バージョンです。アップデートを続行します。"
+    new_ver="v.1.0"
+    fi
+else
+    echo "最新バージョンではありません。再度最新バージョンをインストールします。"
+    fi
+
 echo "Pythonをインストールしています"
 sudo apt-get update && sudo apt-get reinstall -y python-3.7
 echo "MCRPi.pyを再ダウンロードしています"
@@ -9,4 +18,12 @@ echo "ゲームモニターを再ダウンロードしています"
 wget -O GameMonitor.py https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/Python-v.1.0/Utility/${lang}/GameMonitor.py
 echo "アップデートに間違いがないかチェックしています"
 echo "MCRPi.py"
-MCRPipythonver=${cat MCRPi.py.version}
+Mver=${cat MCRPi.py.version}
+if [ "$Mver" = "$new_ver" ]; then
+    echo "[OK] MCRPi.py"
+    lang=jp
+    fi
+else
+    echo "[Fail] MCRPi.py"
+    lang=en
+    fi
