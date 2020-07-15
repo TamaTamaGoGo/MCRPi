2a37d18e9ae92d0800cf328ae46ea06f0e9df2b4
TamaTamaGoGo <tamatama@tensi.org>
2020-07-15 16:39:11 +0900

    Update and rename jpv.1.0Beta.sh to Latest.sh

--- Update/Latest.sh
+++ Update/Latest.sh
@@ -2,12 +2,16 @@
 
 echo "アップデート中です"
 echo "最新バージョンか確認しています"
+wget -O latest.txt https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/Python-Latest/Update/LatestVersion.txt
+latest_ver=$(cat latest.txt)
 if [ "$latest_ver" = "v.1.0" ]; then
     echo "最新バージョンです。アップデートを続行します。"
     new_ver="v.1.0"
     fi
 else
-    echo "最新バージョンではありません。再度最新バージョンをインストールします。"
+    echo "最新バージョンではありません。最新バージョンをインストールします。"
+    wget -O Latest.sh https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/Latest/Update/Latest.sh
+    ./Latest.sh
     fi
 
 echo "Pythonをインストールしています"
