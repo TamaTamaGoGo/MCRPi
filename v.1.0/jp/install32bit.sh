f3e94625c5655824c7fcb560e6b0d9f269f001fe
TamaTamaGoGo <tamatama@tensi.org>
2020-07-21 08:46:28 +0900

    Update install32bit.sh

--- v.1.0/jp/install32bit.sh
+++ v.1.0/jp/install32bit.sh
@@ -11,28 +11,24 @@
   sleep 1
   dir=$PWD
 
-  echo "# $dirにディレクトリーを作成しています"
+  echo "$dirにディレクトリーを作成しています"
   ##zenity --progress --time-remaining --text='Install and Initialize MCPi \nIt only takes minutes \nPlease wait...'
   cd "$dir"
-  echo "2"
   mkdir MCRPi
-  sleep 1
-  echo "4"
   mkdir .minecraft
-  sleep 1
-  echo "6"
   mkdir Minecraft
-  sleep 1
-  echo "8"
   mkdir jdk
-  sleep 1
-  echo "10"
 
-  echo "# GitHubからダウンロードスクリプトをダウンロードしています"
-  sudo wget https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/master/jp/download/download1.sh
-  sudo wget https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/master/jp/download/download2.sh
-  sudo wget https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/master/en/download/download3.sh
+
+  echo "GitHubからダウンロードスクリプトをダウンロードしています"
+#  sudo wget https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/master/jp/download/download1.sh
+#  sudo wget https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/master/jp/download/download2.sh
+#  sudo wget https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/master/en/download/download3.sh
   #cd Minecraft
+  wget -O download https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi/release/v1.0/install/installlist.sh
+  sudo chmod +x download
+  echo "ダウンロードスクリプトを実行しています"
+  ./download
 
   ####Downloading MCPi Core from GITHUB's Repository -Nope
   echo "# Minecraftの公式ランチャーをダウンロードしています"
@@ -66,16 +62,16 @@
   fi
   echo "30"
 
-  echo "# Minecraft Server 1.15.2をダウンロードしています"
-  mkdir MinecraftServer
+#  echo "# Minecraft Server 1.15.2をダウンロードしています"
+#  mkdir MinecraftServer
   #cd MinecraftServer
   #lxterminal --working-directory=${dir}/MinecraftServer -e wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --auto-kill --title="Downloading File..."
   #/jdk/jdk1.8.0_241/bin/java -Xmx500M -Xms500M -jar minecraft_server.1.15.2.jar nogui
-  sudo chmod +x download3.sh
-  ./download3.sh
-  echo "35"
+ # sudo chmod +x download3.sh
+ # ./download3.sh
+#  echo "35"
 
   echo "# change GPU memory"
-  sudo bash -c "echo gpu_mem=248>> /boot/config.txt"
-  sudo sed -i -e "s/^gpu_mem=.*/gpu_mem=248/g" /boot/config.txt
+ # sudo bash -c "echo gpu_mem=248>> /boot/config.txt"
+#  sudo sed -i -e "s/^gpu_mem=.*/gpu_mem=248/g" /boot/config.txt
   echo "40"
