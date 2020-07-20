1ac230269f44c7c669e7750f9deb33db528f7c73
TamaTamaGoGo <tamatama@tensi.org>
2020-07-20 19:34:29 +0900

    Update jpv.1.0.sh

--- Update/jpv.1.0.sh
+++ Update/jpv.1.0.sh
@@ -7,7 +7,6 @@ latest_ver=$(cat latest.txt)
 if [ "$latest_ver" = "v.1.0" ]; then
     echo "最新バージョンです。アップデートを続行します。"
     new_ver="v.1.0"
-    fi
 else
     echo "最新バージョンではありません。最新バージョンをインストールします。"
     wget -O Latest.sh https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/Latest/Update/Latest.sh
