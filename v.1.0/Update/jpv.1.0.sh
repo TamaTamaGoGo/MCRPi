b7d048c0760086766ac3dfd76d48f96a1d21ffab
TamaTamaGoGo <tamatama@tensi.org>
2020-07-20 19:23:44 +0900

    Update jpv.1.0.sh

--- v.1.0/Update/jpv.1.0.sh
+++ v.1.0/Update/jpv.1.0.sh
@@ -7,12 +7,12 @@ latest_ver=$(cat latest.txt)
 if [ "$latest_ver" = "v.1.0" ]; then
     echo "最新バージョンです。アップデートを続行します。"
     new_ver="v.1.0"
-    fi
+fi
 else
     echo "最新バージョンではありません。最新バージョンをインストールします。"
     wget -O Latest.sh https://raw.githubusercontent.com/TamaTamaGoGo/MCRPi---MineCraft-for-RaspberryPi/Latest/Update/Latest.sh
     ./Latest.sh
-    fi
+fi
 
 echo "Pythonをインストールしています"
 sudo apt-get update && sudo apt-get reinstall -y python-3.8
@@ -26,8 +26,8 @@ echo "チェック中:MCRPi.py"
 Mver=${cat MCRPi.py.version}
 if [ "$Mver" = "$new_ver" ]; then
     echo "[OK] MCRPi.py"
-    fi
+fi
 else
     echo "[Fail] MCRPi.py"
     echo "バージョンが、$Mverです。$new_verへ、最ダウンロードします。"
-    fi
+fi
